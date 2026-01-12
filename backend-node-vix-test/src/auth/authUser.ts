78e3b870ed0651348eabbe8c039a9717b49b6984
Rafael Lopes <rafaellopes.dev@gmail.com>
2026-01-12 10:56:03 -0300

    feat(auth): implementar JWT e middleware

--- backend-node-vix-test/src/auth/authUser.ts
+++ backend-node-vix-test/src/auth/authUser.ts
@@ -5,24 +5,34 @@ import { STATUS_CODE } from "../constants/statusCode";
 import { verifyToken } from "../utils/jwt";
 import { CustomRequest } from "../types/custom";
 import { user } from "@prisma/client";
+import { UserService } from "../services/UserService";
+
+const userService = new UserService();
 
 export const authUser = async (
   req: CustomRequest<user>,
   res: Response,
   next: NextFunction,
 ) => {
-  const { authorization } = req.headers;
+  const authorization = req.headers.authorization;
+
   if (!authorization) {
     throw new AppError(ERROR_MESSAGE.INVALID_TOKEN, STATUS_CODE.UNAUTHORIZED);
   }
-  const token = authorization.split(" ")[1];
 
-  // const idUser = verifyToken(token);
-  // const user = //
+  const [scheme, token] = authorization.split(" ");
+  if (scheme !== "Bearer" || !token) {
+    throw new AppError(ERROR_MESSAGE.INVALID_TOKEN, STATUS_CODE.UNAUTHORIZED);
+  }
 
-  // if (isInvalidUser) {
-  //   throw new AppError(ERROR_MESSAGE.UNAUTHORIZED, STATUS_CODE.UNAUTHORIZED);
-  // }
-  // req.user = user;
+  const payload = verifyToken(token);
+
+  const foundUser = await userService.getById(payload.idUser);
+
+  if (!foundUser || foundUser.deletedAt || !foundUser.isActive) {
+    throw new AppError(ERROR_MESSAGE.UNAUTHORIZED, STATUS_CODE.UNAUTHORIZED);
+  }
+
+  req.user = foundUser;
   return next();
 };
