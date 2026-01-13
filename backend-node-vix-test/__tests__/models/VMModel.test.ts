ad7c1891e7249a2af13d7199ed4a6bd5e00e793e
Rafael Lopes <rafaellopes.dev@gmail.com>
2026-01-13 15:27:20 -0300

    test: ajusta teste do VMModel para novo schema

--- backend-node-vix-test/__tests__/models/VMModel.test.ts
+++ backend-node-vix-test/__tests__/models/VMModel.test.ts
@@ -13,7 +13,7 @@ describe("vMModel", () => {
     prismaMock.vM.count.mockResolvedValue(0);
     const r = await vMModel.listAll({
       idBrandMaster: 1,
-      query: { limit: 10, page: 1, offset: 1, orderBy: [] },
+      query: { limit: 10, page: 1, offset: 1, orderBy: [], status: null },
     });
     expect(r).toEqual({ totalCount: 0, result: [] });
   });
